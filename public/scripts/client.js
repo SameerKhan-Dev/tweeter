661a4240a48939d5c2ba47bb403f737987fc7666
Sameer Khan <sameerk_786@hotmail.com>
2020-12-17 13:21:14 +0000

    added logic to prevent default submit behaviour of form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,59 +7,68 @@
 console.log("HELLO FROM CLIENT.JS");
 
 $(document).ready(function() {
-// tweets in this case: is the array of tweets objects
+  // tweets in this case: is the array of tweets objects
 
-const renderTweets = function(tweets) {
-  // loops through the tweets
+  const renderTweets = function(tweets) {
+    // loops through the tweets
 
-  for (let x = 0; x < tweets.length; x++) {
-     // calls createTweetElement for each tweet
-    let $tweet = createTweetElement(tweets[x]);
-    // append the new html element into the html document, under the element with id- tweets-container.
-    $("#tweets-container").prepend($tweet);
+    for (let x = 0; x < tweets.length; x++) {
+      // calls createTweetElement for each tweet
+      let $tweet = createTweetElement(tweets[x]);
+      // append the new html element into the html document, under the element with id- tweets-container.
+      $("#tweets-container").prepend($tweet);
+
+    }
+    // takes return value and appends it to the tweets container.
 
   }
-  // takes return value and appends it to the tweets container.
 
-}
+  /*
+    This function will take in a tweet Object and is responsible for returning a 
+    tweet <article> element containing the entire HTML structure of the tweet.
+  */ 
+  // this function just creates a single new HTML element using JQuery and returns it to the caller
 
-/*
-  This function will take in a tweet Object and is responsible for returning a 
-  tweet <article> element containing the entire HTML structure of the tweet.
-*/ 
-// this function just creates a single new HTML element using JQuery and returns it to the caller
+  const createTweetElement = function (tweetObject) {
 
-const createTweetElement = function (tweetObject) {
+  /* Your code for creating the tweet element */
+    //
+    // the tweet object will be in the form shown below from server (in this example look at the structure of tweetData).
+    // you can use the $() JQuery function to create new HTML element or structure etc, even if its nested etc.
+    let $tweet =  
+    `<article>
+      <header>
+        <img src=${tweetObject.user.avatars} alt ="User Image">
+        <div>
+          <span class ="userName">${tweetObject.user.name}</span> 
+          <span class ="tweeterTagName">${tweetObject.user.handle}</span>
+        </div> 
+      </header>
+      <section>
+        <span class ="tweetFromUser">${tweetObject.content.text}</span>
+      </section>
+      <footer>
+        <span>10 days ago</span>
+        <span><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
+      </footer>
+    </article>`;
+    return $tweet;
+  } 
 
- /* Your code for creating the tweet element */
-  //
-  // the tweet object will be in the form shown below from server (in this example look at the structure of tweetData).
-  // you can use the $() JQuery function to create new HTML element or structure etc, even if its nested etc.
-  
-  
-  
-  let $tweet =  
-  `<article>
-    <header>
-      <img src=${tweetObject.user.avatars} alt ="User Image">
-      <div>
-        <span class ="userName">${tweetObject.user.name}</span> 
-        <span class ="tweeterTagName">${tweetObject.user.handle}</span>
-      </div> 
-    </header>
-    <section>
-      <span class ="tweetFromUser">${tweetObject.content.text}</span>
-    </section>
-    <footer>
-      <span>10 days ago</span>
-      <span><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
-    </footer>
-  </article>`;
- 
-    
+// attach a event handler to handle the submit event that gets fired
+// when submitting the form, through use of button for example in this case.
+// i.e Using jQuery we can use event handlers to prevent the existing form submission 
+// and instead submit the form data using an AJAX request (which will do it asynchronously);
 
-  return $tweet;
-} 
+$("#newTweetForm").on("submit", function (event) {
+
+  // Prevents default action of form, which is to perform the submit event.
+  event.preventDefault();
+
+})
+
+
+
 
 // implement renderTweets function:
 
